bb0397f74118a64c14724435afe063b84b537c35
Brian Inca <brianincah10@gmail.com>
2024-05-08 12:05:44 -0500

    Sem06 intent y paso de parámetros

---
+++ DAM_04_06_Intent_Y_PasoDe_Parametros/app/src/main/java/com/example/dam_04_06_intent_y_pasode_parametros/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dam_04_06_intent_y_pasode_parametros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
